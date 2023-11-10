69a5110dc72c8f962e660647b921eb3aa2346f9f
divawidia <wiartha2001@gmail.com>
2023-11-10 14:22:55 +0800

    add animation in registration complete page

--- resources/views/pages/registration_complete.blade.php
+++ resources/views/pages/registration_complete.blade.php
@@ -17,10 +17,10 @@
     >
         <div class="container z-index-common space-bottom">
             <div class="text-center breadcumb-content">
-                <img src="/img/icon/check.svg" width="250px" alt="check">
-                <h1 class="breadcumb-title">Pendaftaran Berhasil</h1>
-                <p class="breadcumb-text" style="font-size: 18px">Selamat, kamu sudah menyelesaikan proses pendaftaran online. Tim Rhapsody Hospitality Development Center akan segera menghubungi kamu melalui email/whatsapp maksimal 3 x 24 jam.</p>
-                <div class="col-12 text-center mt-4">
+                <img src="/img/icon/check.svg" width="250px" alt="check" data-aos="fade-up" data-aos-duration="1000">
+                <h1 class="breadcumb-title" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="50">Pendaftaran Berhasil</h1>
+                <p class="breadcumb-text" style="font-size: 18px" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="100">Selamat, kamu sudah menyelesaikan proses pendaftaran online. Tim Rhapsody Hospitality Development Center akan segera menghubungi kamu melalui email/whatsapp maksimal 3 x 24 jam.</p>
+                <div class="col-12 text-center mt-4" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="150">
                     <a href="{{ route('home') }}" class="vs-btn"><i class="fa-solid fa-house" style="color: #ffffff; margin-right: 10px; margin-bottom: 5px"></i>Kembali Ke Beranda</a>
                 </div>
             </div>
