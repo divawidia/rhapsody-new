76650f627c23a369c890d044e0bc722b0cf00f1a
divawidia <wiartha2001@gmail.com>
2023-11-27 00:00:00 +0000

    feat (buttons component) : create basic button

---
+++ resources/views/components/buttons/basic-button.blade.php
@@ -0,0 +1,3 @@
+<button type="{{ $type }}" class="btn btn-{{ $color }} {{ $size }}" data-bs-toggle="tooltip" data-bs-placement="bottom" title="{{ $tooltipsTitle }}">
+    <i class="bx bx-{{ $icon }} mr-1"></i> {{ $text }}
+</button>
