012c6e538f0b08037e2190b59db170ab206aab4c
divawidia <wiartha2001@gmail.com>
2023-11-28 00:00:00 +0000

    refactor (web route) : update admin routes

--- routes/web.php
+++ routes/web.php
@@ -89,89 +89,86 @@ Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('
 
 Route::get('/waiting-approval', function (){ return view('auth.verify');});
 
-Route::prefix('admin')
-    ->namespace('App\Http\Controllers\Admin')
-    ->middleware(['auth', 'admin'])
-    ->group(function() {
-        Route::get('/', [DashboardController::class, 'index'])->name('admin-dashboard');
-        Route::resource('calon-peserta-executive', CalonMahasiswaController::class);
-        Route::resource('calon-peserta-diploma', CalonPesertaDiplomaController::class);
-        Route::resource('tags', TagController::class);
-        Route::resource('posts', PostController::class);
-        Route::get('posts-dashboard', [PostController::class, 'dashboard'])->name('posts.dashboard');
-        Route::resource('categories', CategoryController::class);
+Route::prefix('admin')->namespace('App\Http\Controllers\Admin')->middleware(['auth', 'admin'])->group(function() {
+    Route::get('/', [DashboardController::class, 'index'])->name('admin-dashboard');
+    Route::resource('calon-peserta-executive', CalonMahasiswaController::class);
+    Route::resource('calon-peserta-diploma', CalonPesertaDiplomaController::class);
+    Route::resource('tags', TagController::class);
+    Route::resource('posts', PostController::class);
+    Route::get('posts-dashboard', [PostController::class, 'dashboard'])->name('posts.dashboard');
+    Route::resource('categories', CategoryController::class);
 
-        Route::post('upload-blog-photo', [PostController::class, 'uploadPhoto'])->name('blog-photo-upload');
-        Route::post('upload-blog-thumbnail', [PostController::class, 'uploadPhotoThumbnail'])->name('blog-thumbnail-upload');
-        Route::get('delete-blog-photo/{id}', [PostController::class, 'deletePhoto'])->name('blog-photo-delete');
+    Route::post('upload-blog-photo', [PostController::class, 'uploadPhoto'])->name('blog-photo-upload');
+    Route::post('upload-blog-thumbnail', [PostController::class, 'uploadPhotoThumbnail'])->name('blog-thumbnail-upload');
+    Route::get('delete-blog-photo/{id}', [PostController::class, 'deletePhoto'])->name('blog-photo-delete');
 
-        Route::get('/contact-forms', [ContactFormController::class, 'index'])->name('contact-forms.index');
+    Route::get('/contact-forms', [ContactFormController::class, 'index'])->name('contact-forms.index');
 
-        Route::resource('users', UserController::class);
+    Route::resource('users', UserController::class);
 
-        Route::get('/profile-setting', [UserController::class, 'setting'])->name('profile-setting.edit');
-        Route::put('/profile-setting/update', [UserController::class, 'updateSetting'])->name('profile-setting.update');
+    Route::get('/profile-setting', [UserController::class, 'setting'])->name('profile-setting.edit');
+    Route::put('/profile-setting/update', [UserController::class, 'updateSetting'])->name('profile-setting.update');
 
-        Route::get('/profile-setting/change-password', [UserController::class, 'changePassword'])->name('change-password');
-        Route::put('/profile-setting/change-password/update', [UserController::class, 'changePasswordUpdate'])->name('change-password.update');
+    Route::get('/profile-setting/change-password', [UserController::class, 'changePassword'])->name('change-password');
+    Route::put('/profile-setting/change-password/update', [UserController::class, 'changePasswordUpdate'])->name('change-password.update');
 
-        Route::prefix('sosialisasi')
-            ->group(function (){
-                Route::resource('siswa-sma-smk', SiswaSmaSmkController::class);
-                Route::resource('sekolah', SekolahController::class);
-                Route::resource('jadwal', JadwalSosialisasiController::class);
-            });
-        Route::prefix('pages')
-            ->group(function (){
-                Route::get('home', [HomePageController::class, 'edit'])->name('home-page-edit');
-                Route::patch('home', [HomePageController::class, 'update'])->name('home-page-update');
+    Route::prefix('sosialisasi')
+        ->group(function (){
+            Route::resource('siswa-sma-smk', SiswaSmaSmkController::class);
+            Route::resource('sekolah', SekolahController::class);
+            Route::resource('jadwal', JadwalSosialisasiController::class);
+        });
+    Route::prefix('pages')
+        ->group(function (){
+            Route::get('home', [HomePageController::class, 'edit'])->name('home-page-edit');
+            Route::patch('home', [HomePageController::class, 'update'])->name('home-page-update');
 
-                Route::get('about', [AboutPageController::class, 'edit'])->name('about-page-edit');
-                Route::patch('about', [AboutPageController::class, 'update'])->name('about-page-update');
+            Route::get('about', [AboutPageController::class, 'edit'])->name('about-page-edit');
+            Route::patch('about', [AboutPageController::class, 'update'])->name('about-page-update');
 
-                Route::get('contact', [ContactPageController::class, 'edit'])->name('contact-page-edit');
-                Route::patch('contact', [ContactPageController::class, 'update'])->name('contact-page-update');
+            Route::get('contact', [ContactPageController::class, 'edit'])->name('contact-page-edit');
+            Route::patch('contact', [ContactPageController::class, 'update'])->name('contact-page-update');
 
-                Route::prefix('facility')
-                    ->group(function (){
-                        Route::get('/', [FacilityPageController::class, 'edit'])->name('facility-page-edit');
-                        Route::patch('/', [FacilityPageController::class, 'update'])->name('facility-page-update');
-                        Route::post('upload-image', [FacilityContentController::class, 'uploadPhoto'])->name('uploadImageViaAjax');
-                        Route::resource('content', FacilityContentController::class);
-                        Route::get('content/photo/delete/{id}', [FacilityContentController::class, 'deletePhoto'])->name('delete-facility-photo');
-                    });
-                Route::resource('programs', ProgramController::class);
+            Route::prefix('facility')
+                ->group(function (){
+                    Route::get('/', [FacilityPageController::class, 'edit'])->name('facility-page-edit');
+                    Route::patch('/', [FacilityPageController::class, 'update'])->name('facility-page-update');
+                    Route::post('upload-image', [FacilityContentController::class, 'uploadPhoto'])->name('uploadImageViaAjax');
+                    Route::resource('content', FacilityContentController::class);
+                    Route::get('content/photo/delete/{id}', [FacilityContentController::class, 'deletePhoto'])->name('delete-facility-photo');
+                });
+            Route::resource('programs', ProgramController::class);
 
-                Route::resource('pelatihan', ProgramContentController::class);
-                Route::prefix('pelatihan')
-                    ->group(function (){
-                        Route::post('upload-image', [ProgramContentController::class, 'uploadPhoto'])->name('upload-programs-photo');
-                        Route::get('delete-photo/{id}', [ProgramContentController::class, 'deletePhoto'])->name('delete-programs-photo');
+            Route::resource('pelatihan', ProgramContentController::class);
+            Route::prefix('pelatihan')
+                ->group(function (){
+                    Route::post('upload-image', [ProgramContentController::class, 'uploadPhoto'])->name('upload-programs-photo');
+                    Route::get('delete-photo/{id}', [ProgramContentController::class, 'deletePhoto'])->name('delete-programs-photo');
 
-                        Route::get('/{pelatihan}/prospek-karir/create', [ProgramCareerSalaryController::class, 'create'])->name('prospek-karir.create');
-                        Route::post('/{pelatihan}/prospek-karir', [ProgramCareerSalaryController::class, 'store'])->name('prospek-karir.store');
-                        Route::get('/{pelatihan}/prospek-karir/{karir}/edit', [ProgramCareerSalaryController::class, 'edit'])->name('prospek-karir.edit');
-                        Route::put('/{pelatihan}/prospek-karir/{karir}', [ProgramCareerSalaryController::class, 'update'])->name('prospek-karir.update');
-                        Route::delete('/{pelatihan}/prospek-karir/{karir}', [ProgramCareerSalaryController::class, 'destroy'])->name('prospek-karir.destroy');
+                    Route::get('/{pelatihan}/prospek-karir/create', [ProgramCareerSalaryController::class, 'create'])->name('prospek-karir.create');
+                    Route::post('/{pelatihan}/prospek-karir', [ProgramCareerSalaryController::class, 'store'])->name('prospek-karir.store');
+                    Route::get('/{pelatihan}/prospek-karir/{karir}/edit', [ProgramCareerSalaryController::class, 'edit'])->name('prospek-karir.edit');
+                    Route::put('/{pelatihan}/prospek-karir/{karir}', [ProgramCareerSalaryController::class, 'update'])->name('prospek-karir.update');
+                    Route::delete('/{pelatihan}/prospek-karir/{karir}', [ProgramCareerSalaryController::class, 'destroy'])->name('prospek-karir.destroy');
 
-                        Route::get('/{pelatihan}/career-company', [ProgramCareerCompanyController::class, 'index'])->name('career-company.index');
-                        Route::get('/{pelatihan}/career-company/create', [ProgramCareerCompanyController::class, 'create'])->name('career-company.create');
-                        Route::post('/{pelatihan}/career-company', [ProgramCareerCompanyController::class, 'store'])->name('career-company.store');
-                        Route::get('/{pelatihan}/career-company/{company}/edit', [ProgramCareerCompanyController::class, 'edit'])->name('career-company.edit');
-                        Route::put('/{pelatihan}/career-company/{company}', [ProgramCareerCompanyController::class, 'update'])->name('career-company.update');
-                        Route::delete('/{pelatihan}/career-company/{company}', [ProgramCareerCompanyController::class, 'destroy'])->name('career-company.destroy');
-                    });
-            });
-        Route::prefix('section')
-            ->group(function (){
-                Route::get('victory', [VictorySectionController::class, 'edit'])->name('victory-section-edit');
-                Route::patch('victory', [VictorySectionController::class, 'update'])->name('victory-section-update');
+                    Route::get('/{pelatihan}/career-company', [ProgramCareerCompanyController::class, 'index'])->name('career-company.index');
+                    Route::get('/{pelatihan}/career-company/create', [ProgramCareerCompanyController::class, 'create'])->name('career-company.create');
+                    Route::post('/{pelatihan}/career-company', [ProgramCareerCompanyController::class, 'store'])->name('career-company.store');
+                    Route::get('/{pelatihan}/career-company/{company}/edit', [ProgramCareerCompanyController::class, 'edit'])->name('career-company.edit');
+                    Route::put('/{pelatihan}/career-company/{company}', [ProgramCareerCompanyController::class, 'update'])->name('career-company.update');
+                    Route::delete('/{pelatihan}/career-company/{company}', [ProgramCareerCompanyController::class, 'destroy'])->name('career-company.destroy');
+                });
+        });
+    Route::prefix('section')
+        ->group(function (){
+            Route::get('victory', [VictorySectionController::class, 'edit'])->name('victory-section-edit');
+            Route::patch('victory', [VictorySectionController::class, 'update'])->name('victory-section-update');
 
-                Route::resource('testimoni', TestimonyController::class);
-                Route::resource('company', AlumnyCompanyController::class);
-                Route::resource('faq', FaqController::class);
-            });
-    });
+            Route::resource('testimoni', TestimonyController::class);
+            Route::resource('company', AlumnyCompanyController::class);
+            Route::resource('faq', FaqController::class);
+        });
+});
 Auth::routes();
 Route::post('login', [LoginController::class, 'login'])->name('login-admin');
 Route::get('logout', [LoginController::class, 'logout'])->name('logout-admin');
