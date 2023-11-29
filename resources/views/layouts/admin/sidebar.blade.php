621d5dc4306821cc5419d6e4f2d25e208c6b1620
divawidia <wiartha2001@gmail.com>
2023-11-29 00:00:00 +0000

    refactor (admin sidebar) : update calon peserta latih menu

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -23,7 +23,6 @@
             <!-- Left Menu Start -->
             <ul class="metismenu list-unstyled" id="side-menu">
                 <li class="menu-title" data-key="t-menu">Dashboard</li>
-
                 <li>
                     <a href="{{ route('admin-dashboard') }}">
                         <i class="bx bx-home-alt icon nav-icon"></i>
@@ -31,8 +30,29 @@
                     </a>
                 </li>
 
-                <li class="menu-title" data-key="t-articles">Blog, News & Events Article</li>
+                <li class="menu-title" data-key="t-articles">Calon Peserta Latih</li>
+                <li>
+                    <a href="{{ route('calon-peserta-diploma.index') }}">
+                        <i class='bx bx-group icon nav-icon' ></i>
+                        <span class="menu-item" data-key="t-posts">Calon Peserta Diploma</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('calon-peserta-executive.index') }}">
+                        <i class='bx bx-group icon nav-icon' ></i>
+                        <span class="menu-item" data-key="t-posts">Calon Peserta Executive Program</span>
+                    </a>
+                </li>
 
+                <li class="menu-title" data-key="t-applications">Forms Pertanyaan</li>
+                <li>
+                    <a href="{{ route('contact-forms.index') }}">
+                        <i class='bx bx-receipt'></i>
+                        <span class="menu-item" data-key="t-sekolah">Pertanyaan Terkirim</span>
+                    </a>
+                </li>
+
+                <li class="menu-title" data-key="t-articles">Blog, News & Events Article</li>
                 <li>
                     <a href="{{ route('posts.index') }}">
                         <i class='bx bx-file icon nav-icon' ></i>
@@ -53,7 +73,6 @@
                 </li>
 
                 <li class="menu-title">Konten Halaman</li>
-
                 <li>
                     <a href="{{ route('home-page-edit') }}">
                         <i class='bx bx-file-blank'></i>
@@ -86,28 +105,24 @@
                 </li>
 
                 <li class="menu-title" data-key="t-applications">Konten Section</li>
-
                 <li>
                     <a href="{{ route('victory-section-edit') }}">
                         <i class='bx bx-file-blank'></i>
                         <span class="menu-item" data-key="t-victory">Victory Section</span>
                     </a>
                 </li>
-
                 <li>
                     <a href="{{ route('testimoni.index') }}">
                         <i class='bx bx-file-blank'></i>
                         <span class="menu-item" data-key="t-testimoni">Testimony Section</span>
                     </a>
                 </li>
-
                 <li>
                     <a href="{{ route('company.index') }}">
                         <i class='bx bx-file-blank'></i>
                         <span class="menu-item" data-key="t-company">Alumny Company Section</span>
                     </a>
                 </li>
-
                 <li>
                     <a href="{{ route('faq.index') }}">
                         <i class='bx bx-file-blank'></i>
@@ -115,26 +130,15 @@
                     </a>
                 </li>
 
-                <li class="menu-title" data-key="t-applications">Forms Pertanyaan</li>
-
-                <li>
-                    <a href="{{ route('contact-forms.index') }}">
-                        <i class='bx bx-receipt'></i>
-                        <span class="menu-item" data-key="t-sekolah">Pertanyaan Terkirim</span>
-                    </a>
-                </li>
-
                 <li class="menu-title">Manajemen Pengguna</li>
-
                 <li>
                     <a href="{{ route('users.index') }}">
-                        <i class="bx bxs-user icon nav-icon"></i>
+                        <i class="bx bx-user icon nav-icon"></i>
                         <span class="menu-item">Pengguna</span>
                     </a>
                 </li>
 
                 <li class="menu-title">Pengaturan Website & Profile LPK</li>
-
                 <li>
                     <a href="{{ route('setting.edit') }}">
                         <i class="bx bx-cog icon nav-icon"></i>
