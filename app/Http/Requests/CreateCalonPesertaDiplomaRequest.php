1d62da079db2547c6c8555af6930f77903f18487
divawidia <wiartha2001@gmail.com>
2023-11-29 00:00:00 +0000

    refactor (create calon peserta diploma request) : update authorize method

--- app/Http/Requests/CreateCalonPesertaDiplomaRequest.php
+++ app/Http/Requests/CreateCalonPesertaDiplomaRequest.php
@@ -5,14 +5,14 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
-class EditCalonPesertaDiplomaRequest extends FormRequest
+class CreateCalonPesertaDiplomaRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return true;
+        return false;
     }
 
     /**
