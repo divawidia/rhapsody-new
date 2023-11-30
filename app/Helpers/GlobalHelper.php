2bba7007aa6e57ccc60d50a3ef04ba9e88eea161
divawidia <wiartha2001@gmail.com>
2023-11-30 00:00:00 +0000

    refactor (global helper) : create program and testimony function

--- app/Helpers/GlobalHelper.php
+++ app/Helpers/GlobalHelper.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Models\Program;
 use App\Models\Setting;
+use App\Models\Testimony;
 
 function getSettingData()
 {
     return Setting::first();
+}
+
+function getProgramData()
+{
+    return Program::with('program_contents')->get();
+}
+
+function getTestimonyData()
+{
+    return Testimony::all();
 }
