66074f004749ed89179a9c0569a6fa070c99552b
divawidia <wiartha2001@gmail.com>
2023-11-30 00:00:00 +0000

    refactor (calon peserta diploma request) : remove authorize method

--- app/Http/Requests/CreateCalonPesertaDiplomaRequest.php
+++ app/Http/Requests/CreateCalonPesertaDiplomaRequest.php
@@ -7,13 +7,6 @@ use Illuminate\Validation\Rule;
 
 class CreateCalonPesertaDiplomaRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
 
     /**
      * Get the validation rules that apply to the request.
