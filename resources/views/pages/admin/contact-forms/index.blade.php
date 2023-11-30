acb08033eddd8b7f16ff23689d13fdcd1e51bf38
divawidia <wiartha2001@gmail.com>
2023-11-30 00:00:00 +0000

    refactor (contact forms index page) : update tables

--- resources/views/pages/admin/contact-forms/index.blade.php
+++ resources/views/pages/admin/contact-forms/index.blade.php
@@ -1,69 +1,61 @@
 @extends('layouts.admin.master')
 @section('title')
-    Contact Forms
+    Pertanyaan Terkirim
 @endsection
 @section('page-title')
-    Contact Forms
+    @yield('title')
 @endsection
 @section('body')
 
     <body>
     @endsection
     @section('content')
-        @if (session('status'))
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
-                <strong>{{ session('status') }}</strong>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <x-alerts/>
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">@yield('title')</h4>
             </div>
-        @endif
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h4 class="card-title">Pertanyaan Form Kontak</h4>
-                    </div>
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-hover mb-0" id="contactFormTable">
-                                <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Nama</th>
-                                    <th>Email</th>
-                                    <th>No. Telp</th>
-                                    <th>Tgl. Dikirim</th>
-                                    <th>Pesan</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
+            <div class="card-body">
+                <x-tables :headers="[
+                    '#',
+                    'Nama',
+                    'Email',
+                    'No Hp/WA',
+                    'Pesan Pertanyaan',
+                    'Tanggal Dikirim',
+                    'Terakhir Diperbarui',
+                    'Aksi'
+                    ]"
+                />
             </div>
         </div>
-        <!-- end row -->
     @endsection
     @push('addon-script')
-        <script src="https://cdn.datatables.net/v/bs5/jq-3.7.0/dt-1.13.8/datatables.min.js"></script>
         <script>
-            // AJAX DataTable
-            var datatable = $('#contactFormTable').DataTable({
-                processing: true,
-                serverSide: true,
-                ordering: true,
-                ajax: {
-                    url: '{!! url()->current() !!}',
-                },
-                columns: [
-                    { data: 'id', name: 'id' },
-                    { data: 'nama', name: 'nama' },
-                    { data: 'email', name: 'email' },
-                    { data: 'no_telp', name: 'no_telp' },
-                    { data: 'pesan', name: 'pesan' },
-                    { data: 'created_at', name: 'created_at' },
-                ]
+            $(document).ready(function () {
+                $('#table').DataTable({
+                    processing: true,
+                    serverSide: true,
+                    ordering: true,
+                    ajax: {
+                        url: '{!! url()->current() !!}',
+                    },
+                    columns: [
+                        {data: 'DT_RowIndex', name: 'DT_RowIndex', orderable: false, searchable: false},
+                        { data: 'nama', name: 'nama' },
+                        { data: 'email', name: 'email' },
+                        { data: 'no_telp', name: 'no_telp' },
+                        { data: 'pesan', name: 'pesan' },
+                        { data: 'created_at', name: 'created_at' },
+                        { data: 'updated_at', name: 'updated_at' },
+                        {
+                            data: 'action',
+                            name: 'action',
+                            orderable: false,
+                            searchable: false
+                        },
+                    ]
+                });
             });
         </script>
     @endpush
