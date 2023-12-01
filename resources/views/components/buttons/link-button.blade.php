efed12beb7fd75aa46c392a63e457a07a8f5d0ee
divawidia <wiartha2001@gmail.com>
2023-12-01 00:00:00 +0000

    refactor (link button component) : update margin icon

--- resources/views/components/buttons/link-button.blade.php
+++ resources/views/components/buttons/link-button.blade.php
@@ -1,3 +1,3 @@
 <a class="btn btn-{{ $color }} {{ $size }}" href="{{ $route }}" data-bs-toggle="tooltip" data-bs-placement="bottom" title="{{ $tooltipsTitle }}">
-    <i class="bx bx-{{ $icon }} mr-1"></i> {{ $text }}
+    <i class="bx bx-{{ $icon }} mx-2"></i> {{ $text }}
 </a>
