d7d9dacb8f973d69b1c2b0b51e16caa66e1d46dc
divawidia <wiartha2001@gmail.com>
2023-12-02 00:00:00 +0000

    feat (admin topbar) : add notification menu

--- resources/views/layouts/admin/topbar.blade.php
+++ resources/views/layouts/admin/topbar.blade.php
@@ -1,23 +1,15 @@
+@php use Illuminate\Support\Facades\Auth; @endphp
 <header id="page-topbar" class="isvertical-topbar">
     <div class="navbar-header">
         <div class="d-flex">
             <!-- LOGO -->
             <div class="navbar-brand-box">
-                <a href="index" class="logo logo-dark">
+                <a href="{{ route('admin-dashboard') }}" class="logo logo-dark">
                     <span class="logo-sm">
-                        <img src="{{ URL::asset('img/logo-sm.png') }}" alt="" height="26">
+                        <img src="{{ Storage::url(getSettingData()->logo_sidebar_sm) }}" alt="" height="26">
                     </span>
                     <span class="logo-lg">
-                        <img src="{{ URL::asset('img/logo-2.png') }}" alt="" height="26">
-                    </span>
-                </a>
-
-                <a href="index" class="logo logo-light">
-                    <span class="logo-lg">
-                        <img src="{{ URL::asset('img/logo-2.png') }}" alt="" height="30">
-                    </span>
-                    <span class="logo-sm">
-                        <img src="{{ URL::asset('img/logo-sm.png') }}" alt="" height="26">
+                        <img src="{{ Storage::url(getSettingData()->logo_sidebar_lg) }}" alt="" height="26">
                     </span>
                 </a>
             </div>
@@ -34,35 +26,64 @@
 
         </div>
 
-{{--        <div class="d-flex">--}}
-{{--            <div class="dropdown d-inline-block">--}}
-{{--                <button type="button" class="btn header-item noti-icon" data-bs-toggle="dropdown" aria-haspopup="true"--}}
-{{--                    aria-expanded="false">--}}
-{{--                    <i class="bx bx-search icon-sm align-middle"></i>--}}
-{{--                </button>--}}
-{{--                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end p-0">--}}
-{{--                    <form class="p-2">--}}
-{{--                        <div class="search-box">--}}
-{{--                            <div class="position-relative">--}}
-{{--                                <input type="text" class="form-control rounded bg-light border-0"--}}
-{{--                                    placeholder="Search...">--}}
-{{--                                <i class="bx bx-search search-icon"></i>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </form>--}}
-{{--                </div>--}}
-{{--            </div>--}}
+        <div class="d-flex">
+            <div class="dropdown d-inline-block">
+                <button type="button" class="btn header-item noti-icon" id="page-header-notifications-dropdown-v"
+                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="bx bx-bell icon-sm align-middle"></i>
+                    @if(auth()->user()->unreadNotifications->count() > 0)
+                        <span class="noti-dot bg-danger rounded-pill">{{auth()->user()->unreadNotifications->count()}}</span>
+                    @endif
+                </button>
+                <div class="dropdown-menu dropdown-menu-xl dropdown-menu-end p-0"
+                     aria-labelledby="page-header-notifications-dropdown-v">
+                    <div class="p-3">
+                        <div class="row align-items-center">
+                            <div class="col">
+                                <h5 class="m-0 font-size-15"> Notifikasi</h5>
+                            </div>
+                            <div class="col-auto">
+                                <a href="{{ route('notifications.mark-all-as-read') }}" class="small fw-semibold text-decoration-underline">Tandai semua sebagai telah dibaca</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div data-simplebar style="max-height: 250px;">
+                        @if(auth()->user()->unreadNotifications->count() > 0)
+                            @foreach (auth()->user()->unreadNotifications as $notification)
+                                <a href="{{ $notification->data['link'] }}" class="text-reset notification-item unread-notification" id="{{ $notification->id }}">
+                                    <div class="d-flex">
+                                        <div class="flex-grow-1">
+                                            <p class="text-muted font-size-13 mb-0 float-end">{{ $notification->created_at->diffForHumans() }}</p>
+                                            <h6 class="mb-1">{{ $notification->data['title'] }}</h6>
+                                            <div>
+                                                <p class="mb-0">{{ $notification->data['message'] }}</p>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </a>
+                            @endforeach
+                        @else
+                            <a href="#" class="text-reset notification-item">
+                                <div class="d-flex">
+                                    <div class="flex-grow-1">
+                                        <p class="mb-0">Tidak ada notifikasi saat ini</p>
+                                    </div>
+                                </div>
+                            </a>
+                        @endif
+                    </div>
+                </div>
+            </div>
+
             <div class="dropdown d-inline-block">
                 <button type="button" class="btn header-item user text-start d-flex align-items-center"
-                    id="page-header-user-dropdown-v" data-bs-toggle="dropdown" aria-haspopup="true"
-                    aria-expanded="false">
-                    @php
-                        $userId = \Illuminate\Support\Facades\Auth::user()->getAuthIdentifier();
-                        $user = \App\Models\User::findOrFail($userId);
-                    @endphp
+                        id="page-header-user-dropdown-v" data-bs-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false">
                     <img class="rounded-circle header-profile-user object-fit-cover"
-                        src="{{ Storage::url($user->photo_url ?? 'assets/user-profile/user-circle.png') }}" alt="Header Avatar">
-                    <span class="d-none d-xl-inline-block ms-2 fw-medium font-size-15">{{ $user->name }}</span>
+                         src="{{ Storage::url(auth()->user()->photo_url ?? 'assets/user-profile/user-circle.png') }}"
+                         alt="Header Avatar">
+                    <span class="d-none d-xl-inline-block ms-2 fw-medium font-size-15">{{ auth()->user()->name }}</span>
                 </button>
                 <div class="dropdown-menu dropdown-menu-end pt-0">
                     <a class="dropdown-item d-flex align-items-center" href="{{ route('profile-setting.edit') }}">
@@ -78,3 +99,27 @@
         </div>
     </div>
 </header>
+
+@push('addon-script')
+    <script>
+        $(document).on('click', '.unread-notification', function() {
+            let notificationId = $(this).attr('id');
+            $.ajax({
+                url: `{{ route('notifications.mark-as-read', ':id') }}`.replace(':id', notificationId),
+                method: 'PATCH',
+                data: {
+                    _token: '{{ csrf_token() }}'
+                },
+                success: function(response) {
+                    if (response.status === 'success') {
+                        console.log(response.message);
+                    }
+                },
+                error: function(xhr) {
+                    console.error('Failed to mark notification as read');
+                    alert('Failed to mark notification as read');
+                }
+            });
+        });
+    </script>
+@endpush
