983c89efc973a1a3cbbc70f695add46ee68fbfba
divawidia <wiartha2001@gmail.com>
2023-12-02 00:00:00 +0000

    refactor (modal testi) : update modal content

--- resources/views/includes/modal_testi.blade.php
+++ resources/views/includes/modal_testi.blade.php
@@ -1,20 +1,34 @@
-<!-- Modal -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content" style="background-color: var(--theme-color3)">
+<div class="modal fade" id="modalTesti" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+        <div class="modal-content py-3 px-3" style="background-color: var(--theme-color3); border-radius: 20px !important;">
             <div class="modal-header">
-                <h5 class="landing-title5 text-center space-top" data-aos="fade-up" data-aos-duration="1000">
-                    Ini Beberapa Kisah Orang yang Sudah
-                    <FONT COLOR="#CE5423">Terbantu</FONT> Dengan Adanya Program Kami
-                </h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-end">
+                        <button type="button" class="btn-close align-items-end" data-bs-dismiss="modal"></button>
+                    </div>
+                    <div class="col-12">
+                        <h5 class="landing-title5 text-center" data-aos="fade-up" data-aos-duration="1000">
+                            Ini Beberapa Kisah Orang yang Sudah
+                            <FONT COLOR="#CE5423">Terbantu</FONT> Dengan Adanya Program Kami
+                        </h5>
+                    </div>
+                </div>
             </div>
             <div class="modal-body">
-                <div class="inner-video-box" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="50">
-                    <iframe class="video-testi" style=" border-radius:30px" width="315" height="560" src="https://www.youtube.com/embed/1gr3YyeO8QM?si=vTr6hW-eyE_kOEDC" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                    {{--                    <a href="https://www.youtube.com/watch?v=_sI_Ps7JSEk" class="play-btn position-center popup-video"><i class="fas fa-play"></i></a>--}}
+                <div class="inner-video-box text-center" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="50">
+                    <iframe class="modal-video-testi" style=" border-radius:30px" src="https://www.youtube.com/embed/1gr3YyeO8QM?si=vTr6hW-eyE_kOEDC" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+                <div class="card_wrapper">
+                    <div class="container text-center">
+                        <div class="row" style="max-width: 700px">
+                            <div class="title-area my-4 wow fadeInUp" data-wow-delay="0.3s">
+                                <h5 class="sec-title text-center" data-aos="fade-up" data-aos-duration="1000">Beberapa Alumni Kami</h5>
+                            </div>
+                            <div class="col-12">
+                                @include('includes.testi-cards')
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
