3c4b015dbb79273d6fff395b8f69dfc9b7560ec9
divawidia <wiartha2001@gmail.com>
2023-12-02 00:00:00 +0000

    refactor (victory section) : update logo and body text

--- resources/views/includes/victory.blade.php
+++ resources/views/includes/victory.blade.php
@@ -7,13 +7,13 @@
     <div class="container">
         <div class="row align-items-center text-center text-lg-start" style="padding-bottom: 150px">
             <div class="col-lg-5 col-xl-6 space-extra text-start px-4">
-                <img class="mb-5" src="/img/logo-victory.png" alt="logo-victory" data-aos="fade-up"
+                <img class="mb-5" src="/img/logo-victory2.png" alt="logo-victory" data-aos="fade-up"
                      data-aos-duration="1000">
                 <h2 class="sec-title3" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="100">
                     {{ $victory->victory_section_title }}
                 </h2>
                 <p class="fs-md text-start" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
-                    {{ strip_tags($victory->victory_section_body) }}
+                {!! $victory->victory_section_body !!}
                 <div class="hero-btns" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
                     <a href="{{ $victory->victory_section_btn_url }}" class="btn vs-btn style6"
                        style="background-color: var(--theme-color); color: white; border: 0; padding: 10px 30px;">
