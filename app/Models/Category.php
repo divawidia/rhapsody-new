c2a867e6969ebb2ba5c15218971df3630884790d
divawidia <wiartha2001@gmail.com>
2024-04-10 10:50:10 +0800

    create category model

---
+++ app/Models/Category.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id', 'post_id','category_name', 'slug', 'status'
+    ];
+
+    protected $hidden = [
+
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function posts()
+    {
+        return $this->belongsTo(Post::class, 'post_id', 'id');
+    }
+}
