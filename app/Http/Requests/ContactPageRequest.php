fba60b23ab7c40789c0ba325ae05cfe9285b22d9
divawidia <wiartha2001@gmail.com>
2024-07-04 11:41:22 +0800

    create ContactPage request validation

---
+++ app/Http/Requests/ContactPageRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ContactPageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'header_title' => ['required'],
+            'header_bg' => ['image'],
+            'header2_title' => ['required'],
+            'header2_subtitle' => ['required'],
+            'jam_kerja' => ['required'],
+            'alamat' => ['image'],
+            'no_telp' => [ 'required'],
+            'email' => ['required', 'email'],
+            'google_maps' => ['required'],
+        ];
+    }
+}
