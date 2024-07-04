79f3d2d49f18835426d1407cb874c93f411187fe
divawidia <wiartha2001@gmail.com>
2024-07-04 11:36:56 +0800

    create ContactPage model

---
+++ app/Models/ContactPage.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use CyrildeWit\EloquentViewable\InteractsWithViews;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ContactPage extends Model
+{
+    use HasFactory;
+    use InteractsWithViews;
+
+    protected $fillable = [
+        'id',
+        'header_title',
+        'header_bg',
+        'header2_title',
+        'header2_subtitle',
+        'jam_kerja',
+        'alamat',
+        'no_telp',
+        'email',
+        'google_maps',
+    ];
+    protected $table = 'contact_page';
+}
