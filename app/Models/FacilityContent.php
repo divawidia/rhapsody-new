7b370b9249c4a335923d19520a05a45fbe3aa6e1
divawidia <wiartha2001@gmail.com>
2024-07-04 14:32:18 +0800

    create FacilityContent Model

---
+++ app/Models/FacilityContent.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use CyrildeWit\EloquentViewable\InteractsWithViews;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FacilityContent extends Model
+{
+    use InteractsWithViews;
+
+    protected $fillable = [
+        'id',
+        'facility_page_id',
+        'home_page_id',
+        'title',
+        'subtitle',
+        'short_description',
+        'long_decription',
+        'icon',
+    ];
+    protected $table = 'facility_contents';
+
+    public function facility_page(){
+        return $this->belongsTo(FacilityPage::class, 'facility_page_id', 'id');
+    }
+    public function home_page(){
+        return $this->belongsTo(Home::class, 'home_page_id', 'id');
+    }
+}
