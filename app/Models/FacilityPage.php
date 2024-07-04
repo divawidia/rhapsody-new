7b205be49af7be23b03475f1fdb133a3d4119d4f
divawidia <wiartha2001@gmail.com>
2024-07-04 14:34:06 +0800

    update FacilityPage Model

--- app/Models/FacilityPage.php
+++ app/Models/FacilityPage.php
@@ -17,4 +17,7 @@ class FacilityPage extends Model
         'header_bg',
     ];
     protected $table = 'facility_page';
+    public function facility_content(){
+        return $this->hasMany(FacilityContent::class, 'facility_page_id', 'id');
+    }
 }
