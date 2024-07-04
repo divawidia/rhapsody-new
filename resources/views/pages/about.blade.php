940978a95b54ba5a2ce001ef7c8de0edfb6a67a4
divawidia <wiartha2001@gmail.com>
2024-07-04 10:38:01 +0800

    update about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -10,7 +10,7 @@
 ============================== -->
     <div
       class="breadcumb-wrapper d-flex align-items-center"
-      style="background: url('/img/bg/top-bg.jpg');
+      style="background: url('{{ Storage::url($aboutData->header_bg ?? '') }}');
             background-repeat: no-repeat;
             background-size: cover;
             overflow: hidden;
@@ -18,189 +18,129 @@
     >
       <div class="container z-index-common">
         <div class="breadcumb-content text-center">
-            <h1 class="breadcumb-title" data-aos="fade-up" data-aos-duration="1000">Tentang Kami<FONT COLOR="#CE5423">.</FONT></h1>
+            <h1 class="breadcumb-title" data-aos="fade-up" data-aos-duration="1000">@php echo $aboutData->header_title @endphp</h1>
         </div>
       </div>
     </div>
-    <!--==============================
-    About Area
-==============================-->
-    <section class="py-5">
-      <div class="container">
-        <div class="row px-4">
-              <div class="col-lg-7 col-xl-8 mb-30 mb-xl-0">
-                  <div class="title-area mb-3 mb-xl-5">
-                      <h2 class="sec-title text-uppercase" data-aos="fade-right" data-aos-duration="1000" data-aos-delay="100"><FONT COLOR="#006687">RHAPSODY HOSPITALITY DEVELOPMENT CENTER</FONT><FONT COLOR="#CE5423">.</FONT></h2>
-                      <span class="sec-subtitle fw-light" data-aos="fade-right" data-aos-duration="1000">
-                                <FONT COLOR="#006687">adalah
-                                lembaga pendidikan dan pelatihan vokasi khusus
-                                di bidang perhotelan dan kapal pesiar,
-                                yang berkomitmen pada moto
-                                </FONT><br>
-                                The New Direction to Grow
+
+    @if($aboutData->section2_status == 1)
+        <section class="py-5">
+            <div class="container">
+                <div class="row px-4">
+                    <div class="col-lg-7 col-xl-8 mb-30 mb-xl-0">
+                        <div class="title-area mb-3 mb-xl-5">
+                            <h2 class="sec-title text-uppercase" data-aos="fade-right" data-aos-duration="1000" data-aos-delay="100">@php echo $aboutData->section2_title @endphp</h2>
+                            <span class="sec-subtitle fw-light" data-aos="fade-right" data-aos-duration="1000">
+                                @php echo $aboutData->section2_subtitle @endphp
                             </span>
-                  </div>
-                <p class="about-text fs-md mt-n1 w-75" data-aos="fade-up-right" data-aos-duration="1000" data-aos-delay="200">
-                    Kami mempersembahkan konsep pembelajaran inovatif yang mengintegrasikan pelatihan pengembangan diri dan keterampilan dasar perhotelan, semuanya disajikan dengan pendekatan yang menyenangkan, penuh eksplorasi dan dilengkapi dengan tips-tips yang dikaji berdasarkan pengalaman puluhan tahun di lapangan.
-                </p>
-                <p class="about-text fs-md mt-n1 w-75" data-aos="fade-up-right" data-aos-duration="1000" data-aos-delay="400">
-                    Tujuan kami adalah membekali generasi z dengan keterampilan komprehensif agar sukses dalam karir, baik di dalam maupun di luar negeri, serta mampu berkontribusi di era industri 4.0.
-                </p>
-              </div>
-            <div class="col-lg-5 col-xl-4 p-0 order-1 order-lg-2 ms-auto py-5 py-lg-0">
-                <img data-aos="fade-up" data-aos-duration="1000" data-aos-delay="150" width="1024" src="/img/hero/about.png">
+                        </div>
+                        <div class="about-text fs-md mt-n1 w-75" data-aos="fade-up-right" data-aos-duration="1000" data-aos-delay="200">
+                            @php echo $aboutData->section2_body @endphp
+                        </div>
+                    </div>
+                    <div class="col-lg-5 col-xl-4 p-0 order-1 order-lg-2 ms-auto py-5 py-lg-0">
+                        <img data-aos="fade-up" data-aos-duration="1000" data-aos-delay="150" width="1024" src="{{ Storage::url($aboutData->section2_img ?? '') }}">
+                    </div>
+                </div>
             </div>
-        </div>
-      </div>
-    </section>
+        </section>
+    @endif
 
-    <section class="py-5" style="background-color: #FAF5F0;">
-      <div class="container">
-        <div class="title-area text-center">
-            <h2 class="sec-title h1 text-uppercase" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="100">KARIR LUAR NEGERI? PASTI RHAPSODY</h2>
-          <span class="sec-subtitle" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">Mengabulkan lebih dari 500 mimpi dan terus bertambah.</span>
-        </div>
-        <div class="mt-5 row">
-          <div class="col-xl-7 px-4" data-aos="fade-up-right" data-aos-duration="1000" data-aos-delay="200">
-            <div class="img-box3">
-
-              <div class="img-1 mega-hover">
-                <img
-                  class="w-100"
-                  src="/img/about/JNR00860 (1) 1.jpg"
-                  alt="About Img"
-                />
-              </div>
-              <div class="shape-dotted jump"></div>
+    @if($aboutData->section3_status == 1)
+        <section class="py-5" style="background-color: #FAF5F0;">
+            <div class="container">
+                <div class="title-area text-center">
+                    <h2 class="sec-title h1 text-uppercase" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="100">{{ $aboutData->section3_title }}</h2>
+                    <span class="sec-subtitle" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">{{ $aboutData->section3_subtitle }}</span>
+                </div>
+                <div class="mt-5 row">
+                    <div class="col-xl-7 px-4" data-aos="fade-up-right" data-aos-duration="1000" data-aos-delay="200">
+                        <div class="img-box3">
+                            <div class="img-1 mega-hover">
+                                <img
+                                    class="w-100"
+                                    src="{{ Storage::url($aboutData->section3_img ?? '') }}"
+                                    alt="About Img"
+                                />
+                            </div>
+                            <div class="shape-dotted jump"></div>
+                        </div>
+                    </div>
+                    <div class="p-5 col-xl-5 col-xl-4 mb-30 mb-xl-0" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
+                        <p class="fs-md mt-n1">
+                            @php echo $aboutData->section3_body @endphp
+                        </p>
+                    </div>
+                </div>
             </div>
+        </section>
+    @endif
 
-          </div>
-          <div class="p-5 col-xl-5 col-xl-4 mb-30 mb-xl-0" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
-
-            <p class="fs-md mt-n1 fw-bold fst-italic">
-                “Siapapun itu, tidak peduli latar belakangnya, asal dia punya tekad..Pasti berhasil kita bawa ke luar negeri.”
-            </p>
-              <p class="fs-md mt-n1 fw-bold fst-italic">
-                  Wayan Parwita (Founder)
-              </p>
-            <p class="fs-md mt-n1">
-                Kami menerima peserta dari berbagai latar belakang dan menjadikan mereka sumber daya manusia yang unggul. Ketahuilah bahwa sinergi yang kuat ini ada berkat tekadmu untuk bekerja di luar negeri dan <b>kami sangat menghargainya.</b>
-            </p>
-          </div>
-
-        </div>
-      </div>
-    </section>
-    <!--==============================
-  Features Area
-  ==============================-->
-    <section class="space-top space-bottom">
-      <div class="container px-3">
-        <div class="row">
-          <div class="col-12 col-lg" data-aos="fade-up" data-aos-duration="1000">
-            <h5 class="left-border h1 px-2 pt-2">Visi</h5>
-            <p>
-                Menjadikan Indonesia sebagai pionir dalam pengembangan sumber daya manusia di bidang hospitality.
-            </p>
-          </div>
-          <div class="col-12 col-lg" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="100">
-            <h5 class="left-border h1 px-2 pt-2">Misi</h5>
-            <div class="list-style1 vs-list">
-                <ul>
-                    <li>Mengembangkan program pelatihan yang inovatif.</li>
-                    <li>Menjadikan keramahtamahan sebagai karakter dasar setiap lulusan.</li>
-                    <li>Mengasah keterampilan sesuai dengan terobosan baru di industri.</li>
-                    <li>Berkolaborasi dengan pemangku kepentingan di industri.</li>
-                    <li>Memperluas akses terhadap pendidikan berkualitas di seluruh Indonesia.</li>
-                </ul>
+    @if($aboutData->visi_misi_section_status == 1)
+        <section class="space-top space-bottom">
+            <div class="container px-3">
+                <div class="row">
+                    <div class="col-12 col-lg" data-aos="fade-up" data-aos-duration="1000">
+                        <h5 class="left-border h1 px-2 pt-2">Visi</h5>
+                        @php echo $aboutData->visi @endphp
+                    </div>
+                    <div class="col-12 col-lg" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="100">
+                        <h5 class="left-border h1 px-2 pt-2">Misi</h5>
+                        <div class="list-style1 vs-list">
+                            @php echo $aboutData->misi @endphp
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <!--==============================
-      Team Area
-  ==============================-->
-    <section class="py-5 text-center" style="background-color: #FAF5F0;">
-        <img src="/img/hero/sdm.png"  data-aos="fade-up" data-aos-duration="1000">
-      <div class="container">
-        <div class="title-area text-center">
-            <p class="fs-md mt-2 mt-lg-n5" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="100">
-                Kamu juga bisa berkontribusi mengharumkan nama bangsa di kancah internasional.<br>
-                <b>Satu langkah, satu mimpi, satu perubahan bersama Rhapsody.</b>
-            </p>
-        </div>
-{{--        <div class="row">--}}
-{{--          <div class="col-12 col-sm border-end" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">--}}
-{{--            <h1 class="goal-text text-center">S.</h1>--}}
-{{--            <p class="goal-desc fw-bold text-center">Strong leadership</p>--}}
-{{--          </div>--}}
-{{--          <div class="col-12 col-sm border-end" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600">--}}
-{{--            <h1 class="goal-text text-center">I.</h1>--}}
-{{--            <p class="goal-desc fw-bold text-center">Integrity</p>--}}
-{{--          </div>--}}
-{{--          <div class="col-12 col-sm border-end" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="700">--}}
-{{--            <h1 class="goal-text text-center">G.</h1>--}}
-{{--            <p class="goal-desc fw-bold text-center">Gentlemanly</p>--}}
-{{--          </div>--}}
-{{--          <div class="col-12 col-sm border-end" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="800">--}}
-{{--            <h1 class="goal-text text-center">N.</h1>--}}
-{{--            <p class="goal-desc fw-bold text-center">Nice looking</p>--}}
-{{--          </div>--}}
-{{--          <div class="col-12 col-sm border-end" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="900">--}}
-{{--            <h1 class="goal-text text-center">A.</h1>--}}
-{{--            <p class="goal-desc fw-bold text-center">Adeptness</p>--}}
-{{--          </div>--}}
-{{--          <div class="col-12 col-sm border-end" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="1000">--}}
-{{--            <h1 class="goal-text text-center">T.</h1>--}}
-{{--            <p class="goal-desc fw-bold text-center">Team work</p>--}}
-{{--          </div>--}}
-{{--          <div class="col-12 col-sm border-end" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="1200">--}}
-{{--            <h1 class="goal-text text-center">U.</h1>--}}
-{{--            <p class="goal-desc fw-bold text-center">Unpretentious</p>--}}
-{{--          </div>--}}
-{{--          <div class="col-12 col-sm border-end" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="1300">--}}
-{{--            <h1 class="goal-text text-center">R.</h1>--}}
-{{--            <p class="goal-desc fw-bold text-center">Responsible in every action</p>--}}
-{{--          </div>--}}
-{{--          <div class="col-12 col-sm border-start" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="1400">--}}
-{{--            <h1 class="goal-text text-center">E.</h1>--}}
-{{--            <p class="goal-desc fw-bold text-center">Empathy</p>--}}
-{{--          </div>--}}
-{{--        </div>--}}
-      </div>
-    </section>
-    <section class="py-5">
-      <div class="container">
-          <div class="title-area text-center py-5">
-              <h2 class="sec-title h1 text-uppercase" data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="top-bottom" data-aos-delay="100">KENAPA RHAPSODY?  </h2>
-              <span class="sec-subtitle" data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="top-bottom" data-aos-delay="200">Dari sekian banyaknya kampus di Indonesia, ini yang menjadi perbedaan utama.</span>
-          </div>
-        <div class="row">
-          <div class="col-sm-6 col-xl-4" data-aos="fade-up" data-aos-duration="1000">
-              <div class="media-style9">
-                  <div class="media-icon"><img src="/img/icon/cruise-ship-navy.svg" alt="icon"></div>
-                  <h5 class="media-title">AGENT INTERNAL</h5>
-                  <p><b>Kami memprioritaskan masa depanmu.</b> Setiap bukaan lowongan dari berbagai negara akan kami umumkan dan kalian hanya tinggal melangkah.</p>
-              </div>
-          </div>
-          <div class="col-sm-6 col-xl-4" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="100">
-              <div class="media-style9">
-                  <div class="media-icon"><img src="/img/icon/overseas-navy.svg" alt="icon"></div>
-                  <h5 class="media-title">PROGRAM MAGANG LUAR NEGERI</h5>
-                  <p>Sembari menunggu umur sebagai syarat, kamu bisa mendaftarkan diri untuk magang di beberapa negara pilihan.</p>
-              </div>
-          </div>
-          <div class="col-sm-6 col-xl-4" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
-              <div class="media-style9">
-                  <div class="media-icon"><img src="/img/icon/interview-navy.svg" alt="icon"></div>
-                  <h5 class="media-title">TECHNICAL INTERVIEW</h5>
-                  <p>Diantara kamu dan mimpimu ada Interview. Kami punya cara ajaib agar kamu bisa menjadi yang paling lihai di ruangan.</p>
-              </div>
-          </div>
-        </div>
-      </div>
-    </section>
+        </section>
+    @endif
+
+    @if($aboutData->section4_status == 1)
+        <section class="py-5 text-center" style="background-color: #FAF5F0;">
+            <img src="{{ Storage::url($aboutData->section4_img ?? '') }}"  data-aos="fade-up" data-aos-duration="1000" alt="SDM Indonesia">
+            <div class="container">
+                <div class="title-area text-center">
+                    <div class="fs-md mt-2 mt-lg-n5" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="100">
+                        @php echo $aboutData->section4_body @endphp
+                    </div>
+                </div>
+            </div>
+        </section>
+    @endif
+
+    @if($aboutData->whyrhapsody_section_status)
+        <section class="py-5">
+            <div class="container">
+                <div class="title-area text-center py-5">
+                    <h2 class="sec-title h1 text-uppercase" data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="top-bottom" data-aos-delay="100">{{ $aboutData->whyrhapsody_section_title }}</h2>
+                    <span class="sec-subtitle" data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="top-bottom" data-aos-delay="200">{{ $aboutData->whyrhapsody_section_subtitle }}</span>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6 col-xl-4" data-aos="fade-up" data-aos-duration="1000">
+                        <div class="media-style9">
+                            <div class="media-icon"><img src="{{ Storage::url($aboutData->whyrhapsody_section_card1_icon ?? '') }}" alt="icon"></div>
+                            <h5 class="media-title">{{ $aboutData->whyrhapsody_section_card1_title }}</h5>
+                            @php echo $aboutData->whyrhapsody_section_card1_body @endphp
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-xl-4" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="100">
+                        <div class="media-style9">
+                            <div class="media-icon"><img src="{{ Storage::url($aboutData->whyrhapsody_section_card2_icon ?? '') }}" alt="icon"></div>
+                            <h5 class="media-title">{{ $aboutData->whyrhapsody_section_card2_title }}</h5>
+                            @php echo $aboutData->whyrhapsody_section_card2_body @endphp
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-xl-4" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
+                        <div class="media-style9">
+                            <div class="media-icon"><img src="{{ Storage::url($aboutData->whyrhapsody_section_card3_icon ?? '') }}" alt="icon"></div>
+                            <h5 class="media-title">{{ $aboutData->whyrhapsody_section_card3_title }}</h5>
+                            @php echo $aboutData->whyrhapsody_section_card3_body @endphp
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    @endif
     @include('includes.company-profile')
     @include('includes.victory')
     @include('includes.brand')
