93b5e5b303e473055a67a66f5e3e5f095ed823e3
divawidia <wiartha2001@gmail.com>
2024-07-05 13:37:16 +0800

    create ProgramPhoto model

---
+++ app/Models/ProgramPhoto.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProgramPhoto extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'id',
+        'photos_url',
+        'alt',
+        'program_content_id',
+    ];
+    protected $table = 'program_career_salaries';
+    public function program_content(){
+        return $this->belongsTo(ProgramContent::class, 'program_content_id', 'id');
+    }
+}
