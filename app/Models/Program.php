74c35144449904bef050e6911dbc8858fc89280b
divawidia <wiartha2001@gmail.com>
2024-07-05 13:22:45 +0800

    create foreign key migration on program content table and create program model

---
+++ app/Models/Program.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Program extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'id',
+        'name',
+        'slug',
+        'status',
+    ];
+    protected $table = 'programs';
+    public function program_contents(){
+        return $this->hasMany(FacilityContent::class, 'program_id', 'id');
+    }
+}
