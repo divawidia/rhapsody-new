e0adce513e3e53644d3e6b5b920b07b040dba262
divawidia <wiartha2001@gmail.com>
2024-07-06 20:11:11 +0800

    create contact form controller

---
+++ app/Http/Controllers/Admin/ContactFormController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ContactFormRequest;
+use App\Models\AlumnyCompany;
+use App\Models\ContactForm;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use RealRashid\SweetAlert\Facades\Alert;
+use Yajra\DataTables\Facades\DataTables;
+
+class ContactFormController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        if (request()->ajax()) {
+            $query = ContactForm::latest()->get();;
+            return Datatables::of($query)->make();
+        }
+        return view('pages.admin.contact-forms.index');
+    }
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ContactFormRequest $request)
+    {
+        $data = $request->validated();
+        ContactForm::create($data);
+        Alert::success('Hore!', 'Pesan pertanyaan berhasil dikirim!');
+        return redirect()->route('contact');
+    }
+}
