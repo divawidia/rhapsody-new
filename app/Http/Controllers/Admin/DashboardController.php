fb3c3ed5455b49bf876c50c8d7c31b2ff9a74403
divawidia <wiartha2001@gmail.com>
2024-07-07 23:08:26 +0800

    create dashboard controller

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\ContactForm;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('pages.admin.dashboard');
+        $artikel = Post::with(['user', 'tags', 'category'])->latest()->get();
+        $contact = ContactForm::latest()->get();
+        return view('pages.admin.dashboard', [
+            'artikel' => $artikel,
+            'contact' => $contact
+        ]);
     }
 }
